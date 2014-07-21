9e4dd81c8699a35bcbcf7822986315c4b7e5b0d0
James Jones <jamjon3@gmail.com>
2014-07-21 12:47:32 -0400

    Initial Push

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name distributionGroupManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the distributionGroupManagerApp
+ */
+angular.module('distributionGroupManagerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
